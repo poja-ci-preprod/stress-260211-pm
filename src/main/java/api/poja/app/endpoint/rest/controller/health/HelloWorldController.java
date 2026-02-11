ad81c7ec7de6183c172edddc19b1be3787b73fd1
poja-ci <jcloudify@mail.hei.school>
2026-02-11 18:33:26 +0000

    feat: hello world 71

--- src/main/java/api/poja/app/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/health/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
   @GetMapping("/hello-world")
   public String helloWorld() {
-    return "Hello world 70";
+    return "Hello world 71";
   }
 }
